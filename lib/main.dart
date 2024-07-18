c9148ce189331a999056d361fb9e12c6dce7f674
vinistanvini464@gmail.com <vinistanvini464@gmail.com>
2024-07-18 12:56:34 +0530

    Title change in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'News App',
+      title: 'NEWS APP',
       theme: ThemeData(
         // This is the theme of your application.
         //
