2ddd77791071eaa9552d826f8587c3ec56813a15
vinistanvini464@gmail.com <vinistanvini464@gmail.com>
2024-07-18 12:22:15 +0530

    Titie change - main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'News App',
       theme: ThemeData(
         // This is the theme of your application.
         //
